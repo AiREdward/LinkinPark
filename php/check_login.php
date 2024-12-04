939d8619dbaaeca20974e25e628406d8b3171d8c
AiREdward <stefano.baso@studenti.unipd.it>
2024-12-04 11:06:05 +0100

    aggiunta funzione di logout

--- php/check_login.php
+++ php/check_login.php
@@ -6,6 +6,10 @@ $response = [
     "logged_in" => isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true
 ];
 
+if ($response['logged_in']) {
+    $response['email'] = $_SESSION['email']; // Aggiungi l'email se l'utente è loggato
+}
+
 header('Content-Type: application/json');
 echo json_encode($response);
 ?>
