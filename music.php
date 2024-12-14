4d8381f6b7940b24848c1069e48289e971258876
agnese-carraro <agnese.carraro@studenti.unipd.it>
2024-12-14 13:02:32 +0100

    music e css

--- music.php
+++ music.php
@@ -8,8 +8,7 @@
         <meta author="linkins"> 
         <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="asset/css/style.css" media="screen">
-        <link rel="stylesheet" href="asset/css/social.css" media="screen">
+        <link rel="stylesheet" href="asset/css/test per music.css" media="screen">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="icon" href="asset/img/favicon.ico" type="image/x-icon">
     </head>
@@ -21,19 +20,26 @@
         <main>
             <h2>Discografia</h2>
 
+            <div class="albumContainer">
             <!-- from zero -->
-            <h3>From Zero</h3>
-            <img src="asset/img/album/From Zero.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/4R6FV9NSzhPihHR0h4pI93" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2024-11-15">15 novembre 2024</time></dd>
-                <dt>Genere</dt>
-                <dd>Metal, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT31M54S">31:54</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">   
+                <div class="albumMain">          
+                    <img src="asset/img/album/From Zero.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/4R6FV9NSzhPihHR0h4pI93" target="_blank" lang="en">Spotify</a></p>
+                </div>
+                <div class="albumInfo">    
+                    <h3>From Zero</h3> 
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2024-11-15">15 novembre 2024</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Metal, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT31M54S">31:54</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>From Zero (Intro) (<time datetime="PT0M22S">0:22</time>)</li>
                         <li>The Emptiness Machine (<time datetime="PT3M10S">3:10</time>)</li>
@@ -47,22 +53,29 @@
                         <li>IGYEIH (<time datetime="PT3M29S">3:29</time>)</li>
                         <li>Good Things Go (<time datetime="PT3M29S">3:29</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
+
 
             <!-- One More Light -->
-            <h3>One More Light</h3>
-            <img src="asset/img/album/One More Light.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/track/3xXBsjrbG1xQIm1xv1cKOt" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2017-05-19">19 maggio 2017</time></dd>
-                <dt>Genere</dt>
-                <dd>Pop, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT35M19S">35:19</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">   
+                <div class="albumMain">
+                    <img src="asset/img/album/One More Light.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/track/3xXBsjrbG1xQIm1xv1cKOt" target="_blank" lang="en">Spotify</a></p>
+                </div>
+                <div class="albumInfo">  
+                    <h3>One More Light</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2017-05-19">19 maggio 2017</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Pop, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT35M19S">35:19</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>Nobody Can Save Me (<time datetime="PT3M45S">3:45</time>)</li>
                         <li>Good Goodbye (feat. Pusha T) (<time datetime="PT3M31S">3:31</time>)</li>
@@ -75,22 +88,28 @@
                         <li>One More Light (<time datetime="PT4M15S">4:15</time>)</li>
                         <li>Sharp Edges (<time datetime="PT2M58S">2:58</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
 
             <!-- living things -->
-            <h3>Living Things</h3>
-            <img src="asset/img/album/Living Things.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/4XHIjbhjRmqWlosjj5rqSI" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2012-06-20">20 giugno 2012</time></dd>
-                <dt>Genere</dt>
-                <dd>Metal, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT36M59S">36:59</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">
+                <div class="albumMain">
+                    <img src="asset/img/album/Living Things.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/4XHIjbhjRmqWlosjj5rqSI" target="_blank" lang="en">Spotify</a></p>        
+                </div>
+                <div class="albumInfo">
+                    <h3>Living Things</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2012-06-20">20 giugno 2012</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Metal, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT36M59S">36:59</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>Lost in the Echo (<time datetime="PT3M25S">03:25</time>)</li>
                         <li>In My Remains (<time datetime="PT3M20S">3:20</time>)</li>
@@ -105,22 +124,28 @@
                         <li>Tinfoil (<time datetime="PT1M11S">1:11</time>)</li>
                         <li>Powerless (<time datetime="PT3M43S">3:43</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
 
             <!-- A Thousand Suns -->
-            <h3>A Thousand Suns</h3>
-            <img src="asset/img/album/A Thousand Suns.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/5uvXx5ZQswNRFCdHR521YZ" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2010-09-08">8 settembre 2010</time></dd>
-                <dt>Genere</dt>
-                <dd>Rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT47M48S">47:48</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">
+                <div class="albumMain">
+                    <img src="asset/img/album/A Thousand Suns.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/5uvXx5ZQswNRFCdHR521YZ" target="_blank" lang="en">Spotify</a></p>
+                </div>
+                <div class="albumInfo">
+                    <h3>A Thousand Suns</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2010-09-08">8 settembre 2010</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT47M48S">47:48</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>The Requiem (<time datetime="PT3M01S">3:01</time>)</li>
                         <li>The Radiance (<time datetime="PT0M57S">0:57</time>)</li>
@@ -138,22 +163,29 @@
                         <li>The Catalyst (<time datetime="PT5M39S">5:39</time>)</li>
                         <li>The Messenger (<time datetime="PT3M01S">3:01</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
+
 
             <!-- Minutes to Midnight -->
-            <h3>Minutes to Midnight</h3>
-            <img src="asset/img/album/Minutes to Midnight.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/2tlTBLz2w52rpGCLBGyGw6" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2007-05-17">17 maggio 2007</time></dd>
-                <dt>Genere</dt>
-                <dd>Metal, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT43M23S">43:23</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">
+                <div class="albumMain">
+                    <img src="asset/img/album/Minutes to Midnight.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/2tlTBLz2w52rpGCLBGyGw6" target="_blank" lang="en">Spotify</a></p>    
+                </div>
+                <div class="albumInfo">
+                    <h3>Minutes to Midnight</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2007-05-17">17 maggio 2007</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Metal, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT43M23S">43:23</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>Wake (<time datetime="PT1M41S">1:41</time>)</li>
                         <li>Given Up (<time datetime="PT1M41S">1:41</time>)</li>
@@ -168,22 +200,29 @@
                         <li>In Pieces (<time datetime="PT3M29S">3:29</time>)</li>
                         <li>The Little Things Give You Away (<time datetime="PT6M24S">6:24</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
+
 
             <!-- Meteora -->
-            <h3>Meteora</h3>
-            <img src="asset/img/album/Meteora.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/4Gfnly5CzMJQqkUFfoHaP3" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2003-03-25">25 marzo 2003</time></dd>
-                <dt>Genere</dt>
-                <dd>Metal, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT36M35S">36:35</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">
+                <div class="albumMain">
+                    <img src="asset/img/album/Meteora.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/4Gfnly5CzMJQqkUFfoHaP3" target="_blank" lang="en">Spotify</a></p>  
+                </div>
+                <div class="albumInfo">
+                    <h3>Meteora</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2003-03-25">25 marzo 2003</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Metal, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT36M35S">36:35</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>Foreword (<time datetime="PT0M13S">0:13</time>)</li>
                         <li>Don't Stay (<time datetime="PT3M07S">3:07</time>)</li>
@@ -199,22 +238,29 @@
                         <li>Session (Instrumental) (<time datetime="PT2M23S">2:23</time>)</li>
                         <li>Numb (<time datetime="PT3M07S">3:07</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
+
 
             <!-- Hybrid Theory -->
-            <h3>Hybrid Theory</h3>
-            <img src="asset/img/album/Hybrid Theory.jpeg" alt="">
-            <p>Ascolta su <a href="https://open.spotify.com/album/6PFPjumGRpZnBzqnDci6qJ" target="_blank" lang="en">Spotify</a></p>
-            <dl>
-                <dt>Data di pubblicazione</dt> 
-                <dd><time datetime="2000-10-24">24 ottobre 2000</time></dd>
-                <dt>Genere</dt>
-                <dd>Metal, rock</dd>
-                <dt>Durata</dt>
-                <dd><time datetime="PT37M45S">37:45</time></dd>
-                <dt>Tracklist</dt>
-                <dd>
+            <div class="albumCard">
+                <div class="albumMain">
+                    <img src="asset/img/album/Hybrid Theory.jpeg" alt="">
+                    <p>Ascolta su <a href="https://open.spotify.com/album/6PFPjumGRpZnBzqnDci6qJ" target="_blank" lang="en">Spotify</a></p>      
+                </div>
+                <div class="albumInfo">
+                    <h3>Hybrid Theory</h3>
+                    <dl>
+                        <dt>Data di pubblicazione</dt> 
+                        <dd><time datetime="2000-10-24">24 ottobre 2000</time></dd>
+                        <dt>Genere</dt>
+                        <dd>Metal, rock</dd>
+                        <dt>Durata</dt>
+                        <dd><time datetime="PT37M45S">37:45</time></dd>
+                    </dl>
+                </div>
+                <div class="tracklist">
+                    <h4>Tracce</h4>
                     <ol>
                         <li>Papercut (<time datetime="PT3M04S">3:04</time>)</li>
                         <li>One Step Closer (<time datetime="PT2M35S">2:35</time>)</li>
@@ -229,8 +275,9 @@
                         <li>Cure for the Itch (<time datetime="PT2M37S">2:37</time>)</li>
                         <li>Pushing Me Away (<time datetime="PT3M11S">3:11</time>)</li>
                     </ol>
-                </dd>
-            </dl>
+                </div>
+            </div>
+            </div>
         </main>
 
         <?php include 'includes/footer.php'; ?>
