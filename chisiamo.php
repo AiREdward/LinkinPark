247d3579a3e3b6ed1b65ad99b97b1f51a00f6ddb
Sara <ristovicsara999@gmail.com>
2024-12-22 05:35:06 +0100

    cambiati i nomi delle foto dei membri; aggiunto il contenuto in chisiamo.php

--- chisiamo.php
+++ chisiamo.php
@@ -18,67 +18,105 @@
         
         <main>
             <h2>Chi siamo</h2>
-            <p>coaimgniatvheiornhvmcigrapimercghiauhcvbIPNMCiNAVONLAKBOKonpmaercmgohuaomeghgaegahaha</p>
-            <p>coaimgniatvheiornhvmcigrapimercghiauhcvbIPNMCiNAVONLAKBOKonpmaercmgohuaomeghgaegahaha</p>
-            <p>coaimgniatvheiornhvmcigrapimercghiauhcvbIPNMCiNAVONLAKBOKonpmaercmgohuaomeghgaegahaha</p>
+            <p>Siamo i Linkin Park, una band nata nel 1996 con l’obiettivo di mescolare rock, nu-metal e sonorità elettroniche. Il nostro primo album, Hybrid Theory, 
+                ci ha portato al successo globale, e da allora abbiamo continuato a sperimentare, spaziando tra generi e innovando il nostro stile. Con brani come 
+                In the End e Numb, raccontiamo storie di emozioni profonde e temi universali. Siamo qui per condividere con voi la nostra musica e la nostra passione, 
+                perché siete da sempre il cuore del nostro viaggio.</p>
 
             <h2>Integranti</h2>
+
             <dl id="integranti">
-                <dt>Arran Black</dt>
-                <dd>
+
+                <dt>Dave Farrell</dt>
+                <dd><img src="asset/img/integranti/Dave_Farrell.webp" alt="TO DO"></dd>     <!--perche' non si apre la foto?-->
+                <dd>    <!--metto piu' <dd> oppure uno solo e poi dentro una <ul> con tutte le info? -->
                     <ul>
-                        <li>Batterista</li>
-                        <li>...</li>
-                        <li>...</li>
-                        <li>...</li>
+                        <li>Stage name: Phoenix</li>
+                        <li>Ruolo: bassista</li>
+                        <li>Dal: 1996</li>
+                            <!--nella descrizione non ripeterei di nuovo il nome-->
+                        <li><p>Dave "Phoenix" Farrell: Nato nel 1977 a Plymouth, Massachusetts, è il bassista della band. Trasferitosi in California, ha sviluppato il 
+                            suo talento con vari strumenti. Prima di unirsi ai Linkin Park, ha suonato in altre band, portando con sé esperienza e carisma.</p></li>
                     </ul>
                 </dd>
 
-                <dt>Anthony Harcombe</dt>
+                <dt>Brad Delson</dt>
+                <dd><img src="asset/img/integranti/Brad_Delson.webp" alt="TO DO"></dd>
                 <dd>
                     <ul>
-                        <li>Bassista</li>
-                        <li>...</li>
-                        <li>...</li>
-                        <li>...</li>
+                        <li>Ruolo: chitarrista principale</li>
+                        <li>Dal: 1996</li>
+                        <li><p>Brad Delson: Nato nel 1977 ad Agoura Hills, California, è il chitarrista principale. Amico d'infanzia di Mike Shinoda, condivideva con lui il sogno 
+                            di fondare una band. Riconoscibile per le sue cuffie sul palco, si occupa anche del management della band.</p></li>
                     </ul>
                 </dd>
 
-                <dt>Chris Ennis</dt>
+                <dt>Joe Hahn</dt>
+                <dd><img src="asset/img/integranti/Joe_Hahn.webp" alt="TO DO"></dd>
                 <dd>
                     <ul>
-                        <li>Cantante</li>
-                        <li>...</li>
-                        <li>...</li>
-                        <li>...</li>
+                        <li>Ruolo: DJ e direttore creativo</li>
+                        <li>Dal: 1996</li>
+                        <li><p>Joe Hahn: Nato nel 1977 a Dallas, Texas, è il DJ e regista della band. Di origini coreane, è appassionato di arti visive e spesso cura i video 
+                            musicali dei Linkin Park. Il suo stile unico aggiunge un tocco elettronico distintivo alle loro canzoni.</p></li>
                     </ul>
                 </dd>
 
-                <dt>Nathaniel Lue</dt>
+                <dt>Emily Armstrong</dt>
+                <dd><img src="asset/img/integranti/Emily_Armstrong.webp" alt="TO DO"></dd>
                 <dd>
                     <ul>
-                        <li>Chitarrista</li>
-                        <li>...</li>
-                        <li>...</li>
-                        <li>...</li>
+                        <li>Ruolo: cantante principale</li>
+                        <li>Dal: 2024</li>
+                        <li><p>Emily Armstrong: Nata nel 1986 è la nuova voce dei Linkin Park dal 2024. Nata a Los Angeles, è conosciuta per il suo lavoro come cantante dei Dead Sara. 
+                            La sua potente voce e il  carisma sul palco hanno attirato l’attenzione della band, che l'ha scelta per il nuovo corso dopo la perdita di 
+                            Chester Bennington. Il suo debutto con i Linkin Park avviene nell’album From Zero, segnando una nuova era per il gruppo.</p></li>
                     </ul>
                 </dd>
+
+                <dt>Colin Brittain</dt>
+                <dd><img src="asset/img/integranti/Colin_Brittain.webp" alt="TO DO"></dd>
+                <dd>
+                    <ul>
+                        <li>Ruolo: batterista</li>
+                        <li>Dal: 2024</li>
+                        <li><p>Colin Brittain (all’anagrafe “Colin Cunningham”) è un musicista, produttore e compositore nato nel 1986 a Nashville, Tennessee. 
+                            Ha iniziato come batterista per la band Oh No Fiasco e successivamente ha collaborato con artisti come Papa Roach, 5 Seconds of Summer 
+                            e A Day to Remember. Dal 2024, è il nuovo batterista dei Linkin Park, scelto per il suo talento e la sintonia con la band.</p></li>
+                    </ul>
+                </dd>
+
+                <dt>Mike Shinoda</dt>
+                <dd><img src="asset/img/integranti/Mike_Shinoda.webp" alt="TO DO"></dd>
+                <dd>
+                    <ul>
+                        <li>Ruolo: cantante, rapper, chitarrista, pianista, scrittore principale e produttore</li>
+                        <li>Dal: 1996</li>
+                        <li><p>Mike Shinoda: Nato nel 1977 a Panorama City, California, è co-vocalist, rapper e polistrumentista. Di origini giapponesi, ha studiato 
+                            arte e design, influenzando anche l'estetica della band. È la mente dietro molti arrangiamenti ed è il cuore creativo del gruppo.</p></li>
+                    </ul>
+                </dd>
+                
+                <h2>Membri Passati</h2>
+
+            </dl>
+
+            <dl id="ex-integranti">
+                <!--per ogni persona ci sono due <dd> - uno per la foto, uno per la descrizione. Non c'e' la lista. -->
+
+                <dt>Chester Bennington</dt>
+                <dd><img src="asset/img/integranti/Chester_Bennington.webp" alt="TO DO"></dd>
+                <dd><p>Chester Bennington: Nato nel 1976 a Phoenix, Arizona, è stato il carismatico cantante del gruppo. Cresciuto con una passione per la 
+                        musica, ha affrontato difficoltà personali trasformandole in testi profondi. La sua energia sul palco e la voce unica hanno definito il 
+                        sound della band.</p></dd>
+
+                <dt>Rob Bourdon</dt>
+                <dd><img src="asset/img/integranti/Rob_Bourdon.webp" alt="TO DO"></dd>  <!--da scaricare la foto! -->
+                <dd><p>Rob Bourdon: Nato nel 1979 a Calabasas, California, è stato il batterista del gruppo. Ha iniziato a suonare la batteria da bambino, ispirato 
+                    da Aerosmith. La sua precisione e versatilità sono fondamentali per il sound della band.</p></dd>
+                
             </dl>
             
-            <h2>Premi</h2>
-            <dl id="premi">
-                <dt><time datetime="2024-11-01">1 novembre 2024</time>:</dt>
-                <dd>descrizione ultimo premio</dd>
-
-                <dt><time datetime="2024-10-31">31 ottobre 2024</time>:</dt>
-                <dd>descrizione premio</dd>
-
-                <dt><time datetime="2024-10-30">30 ottobre 2024</time>:</dt>
-                <dd>descrizione premio</dd>
-
-                <dt><time datetime="2024-10-29">29 ottobre 2024</time>:</dt>
-                <dd>descrizione premio</dd>
-            </dl>
         </main>
 
         <?php include 'includes/footer.php'; ?>
