083c21001570cd4ab0da4e6c953b476fc18ef180
AiREdward <stefano.baso@studenti.unipd.it>
2025-01-26 18:18:29 +0100

    spostato css di breadcrumb in file separato + fix colore

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,31 +1,7 @@
-<style>
-    :root{
-        --txtcolor: #EEE0F1;
-    }
-
-    nav[aria-label="breadcrumb"] p {
-        font-size: 16px;
-    }
-
-    nav[aria-label="breadcrumb"] a {
-        text-decoration: none;
-        color: #007bff;
-    }
-
-    nav[aria-label="breadcrumb"] a:hover {
-        text-decoration: underline;
-    }
-
-    nav[aria-label="breadcrumb"] .active {
-        font-weight: bold;
-        color: var(--txtcolor);
-        pointer-events: none;
-        text-decoration: none;
-    }
-</style>
+<link rel="stylesheet" href="asset/css/breadcrumb.css" media="all">
 
 <nav aria-label="breadcrumb">
-    <p>Ti trovi in
+    <p>Ti trovi in:
         <?php
         // Array del percorso: ogni elemento rappresenta una voce della breadcrumb
         $breadcrumb = isset($breadcrumb) ? $breadcrumb : [];
